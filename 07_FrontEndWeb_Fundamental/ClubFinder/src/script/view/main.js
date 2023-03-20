041130a268c48c5003aaee3830d34595bbdb8564
rifkyhilman <rifkyhilman265@gmail.com>
2023-03-20 21:51:53 +0700

    change function reguler to function arrow

--- 07_FrontEndWeb_Fundamental/ClubFinder/src/script/view/main.js
+++ 07_FrontEndWeb_Fundamental/ClubFinder/src/script/view/main.js
@@ -1,24 +1,24 @@
-const main = function () {
+const main = () => {
   const searchElement = document.querySelector('#searchElement');
   const buttonSearchElement = document.querySelector('#searchButtonElement');
   const clubListElement = document.querySelector('#clubList');
 
-  const onButtonSearchClicked = function () {
+  const onButtonSearchClicked = () => {
     const dataSource = new DataSource(renderResult, fallbackResult);
     dataSource.searchClub(searchElement.value);
   };
 
-  const renderResult = function (results) {
+  const renderResult = results => {
     clubListElement.innerHTML = '';
     results.forEach(function (club) {
-      const name = club.name;
-      const fanArt = club.fanArt;
-      const description = club.description;
+
+      const {name, fanArt, description} = club;
 
       const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
 
-      clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+      clubElement.innerHTML = 
+          `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
           <div class="club-info">
           <h2> ${name} </h2>
           <p> ${description} </p>
@@ -27,7 +27,7 @@ const main = function () {
     });
   };
 
-  const fallbackResult = function (message) {
+  const fallbackResult = message => {
     clubListElement.innerHTML = '';
     clubListElement.innerHTML += `<h2 class="placeholder"> ${message} </h2>`;
   };
