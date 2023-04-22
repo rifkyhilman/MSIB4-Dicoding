458d634f04424d63e75dd615dfe2988349cd1bf1
rifkyhilman <rifkyhilman265@gmail.com>
2023-04-22 20:38:30 +0700

    Fetch API

--- 08_FrontEndWeb-Expert/movie-catalogue/src/scripts/views/pages/detail.js
+++ 08_FrontEndWeb-Expert/movie-catalogue/src/scripts/views/pages/detail.js
@@ -1,13 +1,20 @@
+import UrlParser from '../../routes/url-parser';
+import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
+ 
 const Detail = {
-    async render() {
-      return `
-        <h2>Detail Page</h2>
-      `;
-    },
-   
-    async afterRender() {
-      // Fungsi ini akan dipanggil setelah render()
-    },
-  };
-   
-  export default Detail;
+  async render() {
+    return `
+      <div id="movie" class="movie"></div>
+    `;
+  },
+ 
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    const movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML = createMovieDetailTemplate(movie);
+  },
+};
+ 
+export default Detail;
