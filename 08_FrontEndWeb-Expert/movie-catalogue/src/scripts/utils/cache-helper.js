32376cfde6fb1a7406f7fcc46a41082f4e5a5e70
rifkyhilman <rifkyhilman265@gmail.com>
2023-05-01 23:49:14 +0700

    Cache api setup

---
+++ 08_FrontEndWeb-Expert/movie-catalogue/src/scripts/utils/cache-helper.js
@@ -0,0 +1,16 @@
+const CacheHelper = {
+    async cachingAppShell(requests) {
+ 
+    },
+   
+    async deleteOldCache() {
+   
+    },
+   
+    async revalidateCache(request) {
+   
+    },
+ 
+};
+ 
+export default CacheHelper; 
